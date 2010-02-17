8512a902545a2de33b153c2b5a2be36d844482c7
JulienLajugie <julien.lajugie@gmail.com>
2010-02-17 22:38:04 +0000

    optimization boolean array list : memory divided by 8 !

--- edu/yu/einstein/gdp2/core/list/arrayList/BooleanArrayAsDoubleList.java
+++ edu/yu/einstein/gdp2/core/list/arrayList/BooleanArrayAsDoubleList.java
@@ -10,8 +10,12 @@ import java.util.List;
 
 
 /**
- * An array of booleans encapsulated in order to implement the {@link List} interface with Double parameter
- * It means that the methods get and set work with Double objects
+ * An array of bytes encapsulated in order to implement the {@link List} interface with Double parameter
+ * <br>Each bytes represent 8 booleans. 
+ * <br>This is an optimization in the memory usage since: 
+ * <br>1 byte (representing 8 booleans) = 1 byte in memory
+ * <br>better than 1 boolean = 1 byte in memory
+ * <br>The methods get and set work with Double objects
  * @author Julien Lajugie
  * @version 0.1
  */
@@ -19,7 +23,7 @@ public final class BooleanArrayAsDoubleList extends AbstractList<Double> impleme
 
 	private static final long serialVersionUID = -5280328695672981245L;	// generated ID
 	private static final int 	RESIZE_STEP = 10000;	// length added every time the array is resized
-	private boolean[] 			data;					// boolean data array
+	private byte[] 				data;					// byte data array (8 booleans / byte)
 	private int 				size;					// size of the list
 	
 	
@@ -27,7 +31,7 @@ public final class BooleanArrayAsDoubleList extends AbstractList<Double> impleme
 	 * Creates an instance of {@link BooleanArrayAsDoubleList}
 	 */
 	public BooleanArrayAsDoubleList() {
-		this.data = new boolean[0];
+		this.data = new byte[0];
 		this.size = 0;
 	}
 	
@@ -37,7 +41,9 @@ public final class BooleanArrayAsDoubleList extends AbstractList<Double> impleme
 	 * @param size size of the array
 	 */
 	public BooleanArrayAsDoubleList(int size) {
-		this.data = new boolean[size];
+		// 1 byte = 8 booleans so the size of the byte array is 8 times smaller
+		int realSize = (int)Math.ceil(size / 8) + 1;
+		this.data = new byte[realSize];
 		this.size = size;
 	}
 	
@@ -46,6 +52,7 @@ public final class BooleanArrayAsDoubleList extends AbstractList<Double> impleme
 	 * Unsupported operation
 	 */
 	public void sort() {
+		// no sort with booleans
 		throw (new UnsupportedOperationException("Invalid operation, can't sort a boolean array"));
 	};
 
@@ -53,15 +60,16 @@ public final class BooleanArrayAsDoubleList extends AbstractList<Double> impleme
 	@Override
 	public boolean add(Double e) {
 		// if the array is to small we resize it before adding the data
-		if (size >= data.length) {
-			boolean[] newData = new boolean[data.length + RESIZE_STEP];			
+		if (size >= data.length * 8) {
+			int realResizeStep = (int)Math.ceil(RESIZE_STEP / 8);
+			byte[] newData = new byte[data.length + realResizeStep];			
 			for (int i = 0; i < data.length; i++) {
 				newData[i] = data[i];
 			}
-			data = newData;			
+			data = newData;
 		}
-		// true if e is not zero
-		data[size] = (e != 0d);
+		// we add the data
+		set(size, e);
 		size++;
 		return true;
 	}
@@ -69,7 +77,12 @@ public final class BooleanArrayAsDoubleList extends AbstractList<Double> impleme
 	
 	@Override
 	public Double get(int index) {
-		return (data[index] ? 1d : 0d);
+		// real index is the index divided by 8
+		int realIndex = index / 8;
+		// compute the position of the desired boolean inside the byte
+		int offset = index % 8;
+		boolean result = (data[realIndex] & (1 << offset)) != 0;
+		return (result == true) ? 1d : 0d;
 	}
 
 	
@@ -78,7 +91,17 @@ public final class BooleanArrayAsDoubleList extends AbstractList<Double> impleme
 	 */
 	@Override
 	public Double set(int index, Double element) {
-		data[index] = (element != 0);
+		// real index is the index divided by 8
+		int realIndex = index / 8;
+		// compute the position of the desired boolean inside the byte
+		int offset = index % 8;
+		if (element != 0) {
+			// set the specified bit of the byte to 1
+			data[realIndex] = (byte)(data[realIndex] | (1 << offset));
+		} else {
+			// set the specified bit of the byte to 0
+			data[realIndex] = (byte)(data[realIndex] & (0xff ^ (1 << offset)));
+		}
 		return null;
 	}
 
