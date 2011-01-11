710856c13230c7a16be852e78a0e7ee1bac99ef4
JulienLajugie <julien.lajugie@gmail.com>
2011-01-11 23:27:14 +0000

    moving average function in progress
    Former-commit-id: 44516bb40c4e996fb6133bb1b9ca653c87a5823a

--- edu/yu/einstein/gdp2/core/list/binList/operation/BLOMovingAverage.java
+++ edu/yu/einstein/gdp2/core/list/binList/operation/BLOMovingAverage.java
@@ -1,130 +1,123 @@
-///**
-// * @author Julien Lajugie
-// * @version 0.1
-// */
-//package yu.einstein.gdp2.core.list.binList.operation;
-//
-//import java.util.ArrayList;
-//import java.util.Collection;
-//import java.util.List;
-//import java.util.concurrent.Callable;
-//import java.util.concurrent.ExecutionException;
-//
-//import yu.einstein.gdp2.core.enums.DataPrecision;
-//import yu.einstein.gdp2.core.list.arrayList.ListFactory;
-//import yu.einstein.gdp2.core.list.binList.BinList;
-//import yu.einstein.gdp2.core.operation.Operation;
-//import yu.einstein.gdp2.core.operationPool.OperationPool;
-//
-//
-///**
-// * Applies a moving average filter on the BinList and returns the result in a new BinList.
-// * @author Julien Lajugie
-// * @version 0.1
-// */
-//public class BLOMovingAverage implements Operation<BinList> {
-//
-//	private final BinList 	binList;		// input list
-//	private final int		halfWidth;		// half of the size of the average window
-//	private final boolean	fillNullValues; // true to fill the null values
-//	private boolean			stopped = false;// true if the operation must be stopped
-//	
-//	
-//	/**
-//	 * Creates an instance of {@link BLOMovingAverage}
-//	 * Applies a moving average filter on the BinList and returns the result in a new BinList.
-//	 * @param binList {@link BinList} input binList
-//	 * @param halfWidth half size 
-//	 * @param fillNullValues set to true to fill the null values
-//	 */
-//	public BLOMovingAverage(BinList binList, int halfWidth, boolean fillNullValues) {
-//		this.binList = binList;
-//		this.halfWidth = halfWidth;
-//		this.fillNullValues = fillNullValues;
-//	}
-//	
-//	
-//	@Override
-//	public BinList compute() throws InterruptedException, ExecutionException {
-//		final OperationPool op = OperationPool.getInstance();
-//		final Collection<Callable<List<Double>>> threadList = new ArrayList<Callable<List<Double>>>();
-//		final DataPrecision precision = binList.getPrecision();
-//		final int binSize =  binList.getBinSize();
-//		final int halfWidth = 2 * sigma / binSize;
-//		// we create an array of coefficients. The index correspond to a distance and for each distance we calculate a coefficient 
-//		final double[] coefTab = new double[halfWidth + 1];
-//		for(int i = 0; i <= halfWidth; i++) {
-//			coefTab[i] = Math.exp(-(Math.pow(((double) (i * binSize)), 2) / (2.0 * Math.pow((double) sigma, 2))));
-//		}
-//		// we gauss
-//		for(short i = 0; i < binList.size(); i++) {
-//			final List<Double> currentList = binList.get(i);
-//			Callable<List<Double>> currentThread = new Callable<List<Double>>() {			 	
-//				@Override
-//				public List<Double> call() throws Exception {
-//					List<Double> listToAdd = null;
-//					if ((currentList != null) && (currentList.size() != 0)) {
-//						listToAdd = ListFactory.createList(precision, currentList.size());
-//						for(int j = 0; j < currentList.size() && !stopped; j++) {
-//							if ((currentList.get(j) != 0) || (fillNullValues)) {
-//								// apply the array of coefficients centered on the current value to gauss
-//								double SumCoef = 0;
-//								double SumNormSignalCoef = 0;
-//								for (int k = -halfWidth; k <= halfWidth && !stopped; k++) {
-//									if((j + k >= 0) && ((j + k) < currentList.size()))  {
-//										int distance = Math.abs(k);
-//										if(currentList.get(j + k) != 0)  {
-//											SumCoef += coefTab[distance];
-//											SumNormSignalCoef += coefTab[distance] * currentList.get(j + k);
-//										}
-//									}
-//								}
-//								if(SumCoef == 0) {
-//									listToAdd.set(j, 0d);
-//								} else {
-//									listToAdd.set(j, SumNormSignalCoef / SumCoef);
-//								}
-//							} else {
-//								listToAdd.set(j, 0d);
-//							}
-//						}
-//					}
-//					op.notifyDone();
-//					return listToAdd;
-//				}
-//			};
-//			threadList.add(currentThread);
-//		}
-//		List<List<Double>> result = op.startPool(threadList);
-//		if (result != null) {
-//			BinList resultList = new BinList(binSize, precision, result);
-//			return resultList;
-//		} else {
-//			return null;
-//		}
-//	}
-//
-//	
-//	@Override
-//	public String getDescription() {
-//		return "Operation: Gauss, Sigma = " + sigma + "bp";
-//	}
-//	
-//	
-//	@Override
-//	public int getStepCount() {
-//		return BinList.getCreationStepCount(binList.getBinSize()) + 1;
-//	}
-//	
-//	
-//	@Override
-//	public String getProcessingDescription() {
-//		return "Gaussing";
-//	}
-//
-//	
-//	@Override
-//	public void stop() {
-//		this.stopped = true;
-//	}
-//}
+/**
+ * @author Julien Lajugie
+ * @version 0.1
+ */
+package yu.einstein.gdp2.core.list.binList.operation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+
+import yu.einstein.gdp2.core.enums.DataPrecision;
+import yu.einstein.gdp2.core.list.arrayList.ListFactory;
+import yu.einstein.gdp2.core.list.binList.BinList;
+import yu.einstein.gdp2.core.operation.Operation;
+import yu.einstein.gdp2.core.operationPool.OperationPool;
+
+
+/**
+ * Computes a moving average on the BinList and returns the result in a new BinList.
+ * @author Julien Lajugie
+ * @version 0.1
+ */
+public class BLOMovingAverage implements Operation<BinList> {
+
+	private final BinList 	binList;		// input list
+	private final int		halfWidth;		// half of the size of the average window
+	private final boolean	fillNullValues; // true to fill the null values
+	private boolean			stopped = false;// true if the operation must be stopped
+	
+	
+	/**
+	 * Creates an instance of {@link BLOMovingAverage}
+	 * Computes a moving average on the BinList and returns the result in a new BinList.
+	 * @param binList {@link BinList} input binList
+	 * @param halfWidth half size in bases
+	 * @param fillNullValues set to true to fill the null values
+	 */
+	public BLOMovingAverage(BinList binList, int halfWidth, boolean fillNullValues) {
+		this.binList = binList;
+		this.halfWidth = halfWidth;
+		this.fillNullValues = fillNullValues;
+	}
+	
+	
+	@Override
+	public BinList compute() throws InterruptedException, ExecutionException {
+		final OperationPool op = OperationPool.getInstance();
+		final Collection<Callable<List<Double>>> threadList = new ArrayList<Callable<List<Double>>>();
+		final DataPrecision precision = binList.getPrecision();
+		final int binSize =  binList.getBinSize();
+		final int halfWidthBin = 2 * halfWidth / binSize;
+		// we apply the moving average
+		for(short i = 0; i < binList.size(); i++) {
+			final List<Double> currentList = binList.get(i);
+			Callable<List<Double>> currentThread = new Callable<List<Double>>() {			 	
+				@Override
+				public List<Double> call() throws Exception {
+					List<Double> listToAdd = null;
+					if ((currentList != null) && (currentList.size() != 0)) {
+						listToAdd = ListFactory.createList(precision, currentList.size());
+						for(int j = 0; j < currentList.size() && !stopped; j++) {
+							if ((currentList.get(j) != 0) || (fillNullValues)) {
+								double count = 0;
+								double SumNormSignalCoef = 0;
+								for (int k = -halfWidthBin; k <= halfWidthBin && !stopped; k++) {
+									if((j + k >= 0) && ((j + k) < currentList.size()))  {
+										if(currentList.get(j + k) != 0)  {
+											SumNormSignalCoef += currentList.get(j + k);
+											count++;
+										}
+									}
+								}
+								if(count == 0) {
+									listToAdd.set(j, 0d);
+								} else {
+									listToAdd.set(j, SumNormSignalCoef / count);
+								}
+							} else {
+								listToAdd.set(j, 0d);
+							}
+						}
+					}
+					op.notifyDone();
+					return listToAdd;
+				}
+			};
+			threadList.add(currentThread);
+		}
+		List<List<Double>> result = op.startPool(threadList);
+		if (result != null) {
+			BinList resultList = new BinList(binSize, precision, result);
+			return resultList;
+		} else {
+			return null;
+		}
+	}
+
+	
+	@Override
+	public String getDescription() {
+		return "Operation: Moving Average, Half Width = " + halfWidth + "bp";
+	}
+	
+	
+	@Override
+	public int getStepCount() {
+		return BinList.getCreationStepCount(binList.getBinSize()) + 1;
+	}
+	
+	
+	@Override
+	public String getProcessingDescription() {
+		return "Computing Moving Average";
+	}
+
+	
+	@Override
+	public void stop() {
+		this.stopped = true;
+	}
+}
