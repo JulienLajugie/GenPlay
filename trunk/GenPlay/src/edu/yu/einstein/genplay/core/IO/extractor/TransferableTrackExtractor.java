19fb598a634fb21f10e66920713709add7d102c2
Julien Lajugie <julien.lajugie@gmail.com>
2014-01-24 22:12:14 -0500

    system clipboard part 2

---
+++ trunk/GenPlay/src/edu/yu/einstein/genplay/core/IO/extractor/TransferableTrackExtractor.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * GenPlay, Einstein Genome Analyzer
+ * Copyright (C) 2009, 2014 Albert Einstein College of Medicine
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Authors: Julien Lajugie <julien.lajugie@einstein.yu.edu>
+ *          Nicolas Fourel <nicolas.fourel@einstein.yu.edu>
+ *          Eric Bouhassira <eric.bouhassira@einstein.yu.edu>
+ * 
+ * Website: <http://genplay.einstein.yu.edu>
+ ******************************************************************************/
+package edu.yu.einstein.genplay.core.IO.extractor;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import com.sun.xml.internal.ws.encoding.soap.SerializationException;
+
+import edu.yu.einstein.genplay.core.manager.project.ProjectManager;
+import edu.yu.einstein.genplay.exception.exceptions.IncompatibleAssembliesException;
+import edu.yu.einstein.genplay.gui.clipboard.TransferableTrack;
+import edu.yu.einstein.genplay.gui.track.Track;
+
+
+/**
+ * Extractor for the serialized {@link TransferableTrack}
+ * @author Julien Lajugie
+ */
+public class TransferableTrackExtractor extends Extractor {
+
+	/**
+	 * Creates an instance of {@link TransferableTrackExtractor}
+	 * @param dataFile
+	 * @throws SerializationException
+	 */
+	public TransferableTrackExtractor(File dataFile) throws SerializationException {
+		super(dataFile);
+	}
+
+
+	/**
+	 * @return the track from the file
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws IncompatibleAssembliesException if the serialized track is not compatible with the project
+	 */
+	public Track extractTrack() throws IOException, ClassNotFoundException, IncompatibleAssembliesException {
+		ObjectInputStream ois = null;
+		try {
+			FileInputStream in = new FileInputStream(getDataFile());
+			ois = new ObjectInputStream(in);
+			TransferableTrack transTrack = (TransferableTrack) (ois.readObject());
+			if (transTrack.getAssemblyName().equals(ProjectManager.getInstance().getAssembly().getName())) {
+				throw new IncompatibleAssembliesException("The assembly of the source file is not compatible with the assembly of the project");
+			}
+			return transTrack.getTrackToTransfer();
+		} finally {
+			if (ois != null) {
+				ois.close();
+			}
+		}
+	}
+
+
+	@Override
+	public int getFirstBasePosition() {
+		return 0;
+	}
+
+
+	@Override
+	protected String retrieveDataName(File dataFile) {
+		return null;
+	}
+
+
+	@Override
+	public void setFirstBasePosition(int firstBasePosition) {
+		// do nothing
+	}
+}
---
+++ trunk/GenPlay/src/edu/yu/einstein/genplay/gui/clipboard/TransferableTrack.java
@@ -0,0 +1,153 @@
+/*******************************************************************************
+ * GenPlay, Einstein Genome Analyzer
+ * Copyright (C) 2009, 2014 Albert Einstein College of Medicine
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Authors: Julien Lajugie <julien.lajugie@einstein.yu.edu>
+ *          Nicolas Fourel <nicolas.fourel@einstein.yu.edu>
+ *          Eric Bouhassira <eric.bouhassira@einstein.yu.edu>
+ * 
+ * Website: <http://genplay.einstein.yu.edu>
+ ******************************************************************************/
+package edu.yu.einstein.genplay.gui.clipboard;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.yu.einstein.genplay.core.manager.project.ProjectManager;
+import edu.yu.einstein.genplay.exception.ExceptionManager;
+import edu.yu.einstein.genplay.gui.action.track.TASaveAsImage;
+import edu.yu.einstein.genplay.gui.track.Track;
+
+/**
+ * Transferable track and its assembly for clipboard and cut and paste
+ * @author Julien Lajugie
+ */
+public class TransferableTrack implements Transferable, Serializable {
+
+	/** generated serial ID */
+	public static final long serialVersionUID = -5416305611985714193L;
+
+	/** Tranferable rack data flavor */
+	public static DataFlavor trackDataFlavor = new DataFlavor(TransferableTrack.class, "GenPlay Track");
+
+	private final Track 	trackToTransfer;	// track to transfer
+	private final String 	assemblyName;		// assembly of the track to transfer
+
+
+	/**
+	 * Creates an instance of {@link TransferableTrack}
+	 * @param trackToTransfer {@link Track} to transfer
+	 */
+	public TransferableTrack(Track trackToTransfer) {
+		this.trackToTransfer = trackToTransfer;
+		assemblyName = ProjectManager.getInstance().getAssembly().getName();
+	}
+
+
+	/**
+	 * @return the name of the assembly of the track to transfer
+	 */
+	public String getAssemblyName() {
+		return assemblyName;
+	}
+
+
+	/**
+	 * @return the track to transfer
+	 */
+	public Track getTrackToTransfer() {
+		return trackToTransfer;
+	}
+
+
+	@Override
+	public Object getTransferData(DataFlavor flavor)
+			throws UnsupportedFlavorException, IOException {
+		if (flavor == DataFlavor.javaFileListFlavor) {
+			List<File> fileList = new ArrayList<File>();
+			File file = writeSerializedData();
+			fileList.add(file);
+			return fileList;
+		}
+		if (flavor == DataFlavor.imageFlavor) {
+			return TASaveAsImage.createImage(trackToTransfer);
+		}
+		if (flavor == DataFlavor.stringFlavor) {
+			return trackToTransfer.getName();
+		}
+		if (flavor == trackDataFlavor) {
+			return this;
+		}
+		return null;
+	}
+
+
+	@Override
+	public DataFlavor[] getTransferDataFlavors() {
+		DataFlavor[] flavors = {DataFlavor.imageFlavor, DataFlavor.javaFileListFlavor, DataFlavor.stringFlavor, trackDataFlavor};
+		return flavors;
+	}
+
+
+	@Override
+	public boolean isDataFlavorSupported(DataFlavor flavor) {
+		if (flavor == DataFlavor.imageFlavor) {
+			return true;
+		}
+		if (flavor == DataFlavor.javaFileListFlavor) {
+			return true;
+		}
+		if (flavor == DataFlavor.stringFlavor) {
+			return true;
+		}
+		if (flavor == trackDataFlavor) {
+			return true;
+		}
+		return false;
+	}
+
+
+	/**
+	 * @return a File containing the {@link TransferableTrack} serialized
+	 */
+	private File writeSerializedData() {
+		ObjectOutputStream oos = null;
+		try {
+			File tmpFile = File.createTempFile("genplayTmpTrack", ".gplt");
+			FileOutputStream out = new FileOutputStream(tmpFile);
+			oos = new ObjectOutputStream(out);
+			oos.writeObject(this);
+			oos.flush();
+			return tmpFile;
+		} catch (Exception e) {
+			ExceptionManager.getInstance().caughtException(Thread.currentThread(), e);
+		} finally {
+			try {
+				if (oos != null) {
+					oos.close();
+				}
+			} catch (IOException e) {}
+		}
+		return null;
+	}
+}
